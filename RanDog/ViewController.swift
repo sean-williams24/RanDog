f3a856577cb3ee0c9c0327382104c43d49bc7389
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-07-30 09:38:46 +0100

    Initial Commit

---
+++ RanDog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RanDog
+//
+//  Created by Sean Williams on 30/07/2019.
+//  Copyright © 2019 Sean Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
